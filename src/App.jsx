955749d8d70a8da6e68c44f02769bf3f63964ee6
wahyuanang <wahyuanng@students.amikom.ac.id>
2025-01-12 01:42:07 +0700

    install depedencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import image from "./assets/images/Logo-Volunteerin.jpg";
 
 function App() {
-
   return (
     <>
-      <div>
-        <h1 className="text-blue-500">Testing</h1>
+      <div className="flex justify-center items-center h-screen">
+        <img src={image} alt="" className="w-56"/>
+        <h1 className="text-[#0A3E54] text-center text-7xl font-bold">
+          Volun
+          <span className="text-[#EB6B17]">teerin</span>
+        </h1>
       </div>
     </>
   );
