38f7f616229ae625d30d8ddd8f4c337d66fab04f
wahyuanang <wahyuanng@students.amikom.ac.id>
2025-01-12 01:10:38 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-500">Testing</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
