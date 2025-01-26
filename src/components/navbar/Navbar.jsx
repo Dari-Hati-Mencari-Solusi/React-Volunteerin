61520300a1d2a4ef11cef8daf80b03f83c50eaf2
wahyuanang <wahyuanng@students.amikom.ac.id>
2025-01-26 12:31:56 +0700

    feat:add navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import logo from "../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isKategoriDropdownOpen, setIsKategoriDropdownOpen] = useState(false);
-  const [isHubungiKamiDropdownOpen, setIsHubungiKamiDropdownOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const toggleKategoriDropdown = () => {
-    setIsKategoriDropdownOpen(!isKategoriDropdownOpen);
-  };
-
-  const toggleHubungiKamiDropdown = () => {
-    setIsHubungiKamiDropdownOpen(!isHubungiKamiDropdownOpen);
+  const toggleDropdown = (dropdown) => {
+    if (openDropdown === dropdown) {
+      setOpenDropdown(null);
+    } else {
+      setOpenDropdown(dropdown);
+    }
   };
 
   return (
     <nav className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          {/* Logo */}
-          <div className="flex items-center">
+          <div className="flex items-center pt-2">
             <a className="block text-teal-600" href="#">
               <span className="sr-only">Home</span>
               <img src={logo} alt="logo volunteerin" className="w-48" />
@@ -33,34 +32,26 @@ const Navbar = () => {
           {/* Desktop Navigation */}
           <div className="hidden md:block">
             <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
+              <ul className="flex items-center gap-12 text-md">
                 <li>
                   <a
-                    className="text-black transition hover:text-gray-500/75"
+                    className="text-black transition hover:text-[#0A3E54]"
                     href="#"
                   >
                     Beranda
                   </a>
                 </li>
                 <li>
-                  <a
-                    className="text-black transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    Event
-                  </a>
-                </li>
-                <li>
                   <div className="relative">
                     <button
-                      onClick={toggleKategoriDropdown}
-                      className="text-black transition hover:text-gray-500/75 flex items-center gap-1"
+                      onClick={() => toggleDropdown("event")}
+                      className="text-black transition hover:text-[#0A3E54] flex items-center gap-1"
                     >
-                      Kategori
+                      Event
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className={`h-4 w-4 transition-transform ${
-                          isKategoriDropdownOpen ? "rotate-180" : ""
+                          openDropdown === "event" ? "rotate-180" : ""
                         }`}
                         viewBox="0 0 20 20"
                         fill="currentColor"
@@ -72,26 +63,17 @@ const Navbar = () => {
                         />
                       </svg>
                     </button>
-                    {isKategoriDropdownOpen && (
+                    {openDropdown === "event" && (
                       <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-lg py-2">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                        >
-                          Web Development
-                        </a>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                        >
-                          Mobile Development
-                        </a>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                        >
-                          SEO
-                        </a>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          Semua Event
+                        </Link>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          Lingkungan
+                        </Link>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          Community
+                        </Link>
                       </div>
                     )}
                   </div>
@@ -99,14 +81,14 @@ const Navbar = () => {
                 <li>
                   <div className="relative">
                     <button
-                      onClick={toggleHubungiKamiDropdown}
-                      className="text-black transition hover:text-gray-500/75 flex items-center gap-1"
+                      onClick={() => toggleDropdown("kategori")}
+                      className="text-black transition hover:text-[#0A3E54] flex items-center gap-1"
                     >
-                      Hubungi Kami
+                      Kategori
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className={`h-4 w-4 transition-transform ${
-                          isHubungiKamiDropdownOpen ? "rotate-180" : ""
+                          openDropdown === "kategori" ? "rotate-180" : ""
                         }`}
                         viewBox="0 0 20 20"
                         fill="currentColor"
@@ -118,20 +100,51 @@ const Navbar = () => {
                         />
                       </svg>
                     </button>
-                    {isHubungiKamiDropdownOpen && (
+                    {openDropdown === "kategori" && (
                       <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-lg py-2">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                        >
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          Web Development
+                        </Link>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          Mobile Development
+                        </Link>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                          SEO
+                        </Link>
+                      </div>
+                    )}
+                  </div>
+                </li>
+                <li>
+                  <div className="relative">
+                    <button
+                      onClick={() => toggleDropdown("hubungiKami")}
+                      className="text-black transition hover:text-[#0A3E54] flex items-center gap-1"
+                    >
+                      Hubungi Kami
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`h-4 w-4 transition-transform ${
+                          openDropdown === "hubungiKami" ? "rotate-180" : ""
+                        }`}
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                    {openDropdown === "hubungiKami" && (
+                      <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-lg py-2">
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                           Email
-                        </a>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                        >
+                        </Link>
+                        <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                           Telepon
-                        </a>
+                        </Link>
                       </div>
                     )}
                   </div>
@@ -142,20 +155,13 @@ const Navbar = () => {
 
           {/* Login/Register and Hamburger Button */}
           <div className="flex items-center gap-4">
-            {/* Tombol Masuk dan Daftar hanya untuk desktop */}
             <div className="hidden md:flex gap-4">
-              <a
-                className="rounded-md bg-white border-[1.5px] border-[#0A3E54] px-5 py-2.5 text-sm font-medium text-[#0A3E54] hover:text-white transition ease-in-out duration-300 shadow hover:bg-[#0A3E54]"
-                href="#"
-              >
+              <Link className="rounded-md bg-white border-[1.5px] border-[#0A3E54] px-5 py-2.5 text-sm font-medium text-[#0A3E54] hover:text-white transition ease-in-out duration-300 shadow hover:bg-[#0A3E54]">
                 Masuk
-              </a>
-              <a
-                className="rounded-md bg-[#0A3E54] px-5 py-2.5 text-sm font-medium text-white hover:bg-[#0A3E54]/90"
-                href="#"
-              >
+              </Link>
+              <Link className="rounded-md bg-[#0A3E54] px-5 py-2.5 text-sm font-medium text-white hover:bg-[#0A3E54]/90">
                 Daftar
-              </a>
+              </Link>
             </div>
 
             {/* Hamburger Button */}
@@ -222,34 +228,60 @@ const Navbar = () => {
 
           {/* Mobile Navigation Links */}
           <nav aria-label="Global" className="p-4">
-            <ul className="flex flex-col gap-6 text-sm">
+            <ul className="flex flex-col gap-6 text-md">
               <li>
-                <a
-                  className="transition text-black hover:text-gray-500 duration-200 ease-in-out"
-                  href="#"
-                >
+                <Link className="transition text-black hover:text-[#0A3E54] duration-200 ease-in-out">
                   Beranda
-                </a>
-              </li>
-              <li>
-                <a
-                  className="transition text-black hover:text-gray-500 duration-200 ease-in-out"
-                  href="#"
-                >
-                  Event
-                </a>
+                </Link>
               </li>
               <li>
                 <div className="relative">
                   <button
-                    onClick={toggleKategoriDropdown}
-                    className="w-full transition text-black hover:text-gray-500 duration-200 ease-in-out flex items-center justify-between"
+                    onClick={() => toggleDropdown("event")}
+                    className="w-full transition text-black hover:text-[#0A3E54] duration-200 ease-in-out flex items-center justify-between"
+                  >
+                    <span>Event</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-4 w-4 transition-transform ${
+                        openDropdown === "event" ? "rotate-180" : ""
+                      }`}
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  {openDropdown === "event" && (
+                    <div className="mt-2 bg-white rounded-lg py-2">
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        Semua Event
+                      </Link>
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        Lingkungan
+                      </Link>
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        Community
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </li>
+              <li>
+                <div className="relative">
+                  <button
+                    onClick={() => toggleDropdown("kategori")}
+                    className="w-full transition text-black hover:text-[#0A3E54] duration-200 ease-in-out flex items-center justify-between"
                   >
                     <span>Kategori</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className={`h-4 w-4 transition-transform ${
-                        isKategoriDropdownOpen ? "rotate-180" : ""
+                        openDropdown === "kategori" ? "rotate-180" : ""
                       }`}
                       viewBox="0 0 20 20"
                       fill="currentColor"
@@ -261,26 +293,17 @@ const Navbar = () => {
                       />
                     </svg>
                   </button>
-                  {isKategoriDropdownOpen && (
+                  {openDropdown === "kategori" && (
                     <div className="mt-2 bg-white rounded-lg py-2">
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                      >
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                         Web Development
-                      </a>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                      >
+                      </Link>
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                         Mobile Development
-                      </a>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                      >
+                      </Link>
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                         SEO
-                      </a>
+                      </Link>
                     </div>
                   )}
                 </div>
@@ -288,14 +311,14 @@ const Navbar = () => {
               <li>
                 <div className="relative">
                   <button
-                    onClick={toggleHubungiKamiDropdown}
-                    className="w-full transition text-black hover:text-gray-500 duration-200 ease-in-out flex items-center justify-between"
+                    onClick={() => toggleDropdown("hubungiKami")}
+                    className="w-full transition text-black hover:text-[#0A3E54] duration-200 ease-in-out flex items-center justify-between"
                   >
                     <span>Hubungi Kami</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className={`h-4 w-4 transition-transform ${
-                        isHubungiKamiDropdownOpen ? "rotate-180" : ""
+                        openDropdown === "hubungiKami" ? "rotate-180" : ""
                       }`}
                       viewBox="0 0 20 20"
                       fill="currentColor"
@@ -307,37 +330,25 @@ const Navbar = () => {
                       />
                     </svg>
                   </button>
-                  {isHubungiKamiDropdownOpen && (
+                  {openDropdown === "hubungiKami" && (
                     <div className="mt-2 bg-white rounded-lg py-2">
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                      >
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                         Email
-                      </a>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                      >
+                      </Link>
+                      <Link className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
                         Telepon
-                      </a>
+                      </Link>
                     </div>
                   )}
                 </div>
               </li>
               <li className="flex gap-4 mt-4">
-                <a
-                  className="flex-1 rounded-md text-center bg-white border-2 border-[#0A3E54] px-5 py-2.5 text-sm font-medium text-[#0A3E54] hover:text-white transition ease-in-out duration-300 shadow hover:bg-[#0A3E54]"
-                  href="#"
-                >
+                <Link className="flex-1 rounded-md text-center bg-white border-2 border-[#0A3E54] px-5 py-2.5 text-sm font-medium text-[#0A3E54] hover:text-white transition ease-in-out duration-300 shadow hover:bg-[#0A3E54]">
                   Masuk
-                </a>
-                <a
-                  className="flex-1 rounded-md bg-[#0A3E54] px-5 py-2.5 text-sm font-medium text-white hover:bg-[#0A3E54]/90 text-center"
-                  href="#"
-                >
+                </Link>
+                <Link className="flex-1 rounded-md bg-[#0A3E54] px-5 py-2.5 text-sm font-medium text-white hover:bg-[#0A3E54]/90 text-center">
                   Daftar
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
